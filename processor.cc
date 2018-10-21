4b103789350f1026cd2a954376de21098a06ca00
Xavier Décoret <475285+Xadeck@users.noreply.github.com>
2018-10-22 00:19:19 +0200

    Separate functions to handle correctly unfinished strings

--- processor.cc
+++ processor.cc
@@ -31,26 +31,38 @@ bool Match(const LazyRE2 &re, absl::string_view source, size_t size) {
   return re->Match(source, size, source.size(), RE2::ANCHOR_START, nullptr, 0);
 }
 
-template <bool accept_string, typename T>
+template <typename T>
 void ConsumeUntil(absl::string_view source, size_t *size, const T &condition) {
   *size = 0;
   while (*size < source.size() && !Match(condition, source, *size)) {
-    if (accept_string) {
-      const char delimiter = source[*size];
-      if (delimiter == '\'' || delimiter == '"') {
-        while (++*size < source.size() && source[*size] != delimiter) {
-          if (source[*size] == '\\' &&     //
-              *size + 1 < source.size() && //
-              source[*size + 1] == delimiter) {
-            *size += 2;
-          }
+    ++*size;
+  }
+}
+
+template <typename T>
+void ConsumeCharAndStringUntil(absl::string_view source, size_t *size,
+                               const T &condition) {
+  *size = 0;
+  while (*size < source.size() && !Match(condition, source, *size)) {
+    const char delimiter = source[*size];
+    if (delimiter == '\'' || delimiter == '"') {
+      while (++*size < source.size() && source[*size] != delimiter) {
+        // TODO: handle the case of precending escaped backslash.
+        if (source[*size] == '\\' &&     //
+            *size + 1 < source.size() && //
+            source[*size + 1] == delimiter) {
+          *size += 2;
         }
       }
     }
     ++*size;
   }
+  // This captures when a string is unterminated by an escaped delimiter.
+  // TODO: this is a sign the algorithm above should be rewritten.
+  if (*size > source.size()) {
+    *size = source.size();
+  }
 }
-
 template <size_t N>
 const char *Literal(const char (&literal)[N], size_t *size) {
   *size = N - 1;
@@ -89,17 +101,17 @@ const char *Processor::Read(lua_State *L, size_t *size) {
     }
     return nullptr;
   case Mode::TEXT:
-    ConsumeUntil<false>(source_, size, kOpeningExpressionOrStatement);
+    ConsumeUntil(source_, size, kOpeningExpressionOrStatement);
     return To(Mode::TEXT_END), Consumed(*size);
   case Mode::TEXT_END:
     return To(Mode::BEGIN), Literal("]])", size);
   case Mode::EXPRESSION:
-    ConsumeUntil<true>(source_, size, kClosingExpression);
+    ConsumeCharAndStringUntil(source_, size, kClosingExpression);
     return To(Mode::EXPRESSION_END), Consumed(*size);
   case Mode::EXPRESSION_END:
     return To(Mode::BEGIN), Literal(")", size);
   case Mode::STATEMENT:
-    ConsumeUntil<true>(source_, size, kClosingStatement);
+    ConsumeCharAndStringUntil(source_, size, kClosingStatement);
     return To(Mode::STATEMENT_END), Consumed(*size);
   case Mode::STATEMENT_END:
     return To(Mode::BEGIN), Literal(" ", size);
