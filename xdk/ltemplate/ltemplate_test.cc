614955d8c5fec9794b4a5b364b35f826cd3b4863
Xavier Décoret <xdecoret@xdecoret-macbookair.roam.corp.google.com>
2018-10-28 10:55:33 +0100

    Added template test for statement and sandboxing

--- xdk/ltemplate/ltemplate_test.cc
+++ xdk/ltemplate/ltemplate_test.cc
@@ -13,11 +13,13 @@ namespace ltemplate {
 namespace {
 
 using lua::HasField;
+using lua::IsNil;
 using lua::IsString;
 using lua::Stack;
+using ::testing::_;
 using ::testing::StrEq;
 
-TEST(LTemplateTest, Works) {
+TEST(LTemplateTest, ExpressionsWork) {
   lua::State L;
   lua_newtable(L);
 
@@ -28,6 +30,29 @@ TEST(LTemplateTest, Works) {
               HasField("_", IsString("this is 3(three) words")));
 }
 
+TEST(LTemplateTest, StatementsWork) {
+  lua::State L;
+  lua_newtable(L);
+
+  std::string source = R"LT({% x=3 %}the number {{ x }}.)LT";
+  ASSERT_EQ(dostring(L, source.data(), source.size(), "test"), LUA_OK)
+      << Stack(L);
+  ASSERT_THAT(Stack::Element(L, -1), HasField("_", IsString("the number 3.")));
+}
+
+TEST(LTemplateTest, EvaluationIsSandboxed) {
+  lua::State L;
+  lua_newtable(L);
+  lua_pushinteger(L, 5);
+  lua_setfield(L, -2, "x");
+
+  std::string source = R"LT({% y=x-2 %}the number {{ y }}.)LT";
+  ASSERT_EQ(dostring(L, source.data(), source.size(), "test"), LUA_OK)
+      << Stack(L);
+  ASSERT_THAT(lua_gettop(L), 2);
+  ASSERT_THAT(Stack::Element(L, -1), HasField("_", IsString("the number 3.")));
+  ASSERT_THAT(Stack::Element(L, -2), HasField("y", IsNil()));
+}
 } // namespace
 } // namespace ltemplate
 } // namespace xdk
